73cb2f29e59577e1246198b33f5bd60de6c34cdd
ConnorClay <connor.clay@bellevuecollege.edu>
2025-03-15 21:00:47 -0700

    Update script.js

--- a5/js/script.js
+++ a5/js/script.js
@@ -1,5 +1,5 @@
 const images = [
-    { src: 'images/image1.jpg', alt: 'Description of Image 1', description: 'Image 1 Description' },
+    { src: 'images/image1.jpeg', alt: 'Description of Image 1', description: 'Image 1 Description' },
     { src: 'images/image2.jpg', alt: 'Description of Image 2', description: 'Image 2 Description' },
     { src: 'images/image3.jpg', alt: 'Description of Image 3', description: 'Image 3 Description' },
     { src: 'images/image4.jpg', alt: 'Description of Image 4', description: 'Image 4 Description' },
@@ -8,7 +8,7 @@ const images = [
 
 let currentIndex = 0;
 let timer;
-let timerValue = 10; 
+let timerValue = 8; 
 
 function updateCarousel() {
     const image = document.getElementById('carousel-image');
